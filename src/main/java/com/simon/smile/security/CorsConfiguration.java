c56c65af70632df88b91d2fce7932c8b89bffc24
siyingcheng <283736566@qq.com>
2023-11-09 09:01:30 +0800

    fix can not delete user issue

--- src/main/java/com/simon/smile/security/CorsConfiguration.java
+++ src/main/java/com/simon/smile/security/CorsConfiguration.java
@@ -12,6 +12,6 @@ public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedMethods("*");
     }
 }
